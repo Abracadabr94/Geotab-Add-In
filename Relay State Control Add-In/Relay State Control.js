5a008c6f2e8dbaa79ff660f8f3a761deb3a8af83
Abracadabr94 <162787616+Abracadabr94@users.noreply.github.com>
2024-05-16 09:53:03 +0630

    Update Relay State Control.js

--- Relay State Control Add-In/Relay State Control.js
+++ Relay State Control Add-In/Relay State Control.js
@@ -1,14 +1,16 @@
 {
 	"name": "Relay-State-Control",
 	"supportEmail": "aungthuphyo@myanmarparama.com",
-	"version": "1.0",
+	"version": "1.0.2",
 	"items": [
 		{
-			"url": "",
-			"path": "",
-			"menuName": {
-				"en": ""
-			}
-		}
-	]
+			"url":"https://app.geotab.com/addins/geotab/relay-state-control/1.0.2/output.html",
+			"path":"EngineMaintenanceLink/",
+			"menuName":{
+				"en": "Relay-State-Control"
+		},
+		"icon":"https://app.geotab.com/addins/geotab/relay-state-control/1.0.2/images/icon.jpg"
+						}
+	],
+	"isSigned":false
 }
